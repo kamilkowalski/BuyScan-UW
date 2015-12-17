4a3f7a684b531d07b58f79a955851847d99dcffe
Kamil Kowalski <kowalski.tychy@gmail.com>
2015-12-17 01:15:25 +0100

    Add Receipt and ReceiptItem model, create Mock object

---
+++ BuyScan-UW/Models/Receipt.cs
@@ -0,0 +1,27 @@
+﻿using BuyScan_UW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyScan_UW
+{
+    class Receipt
+    {
+        public DateTime CreatedAt { get; set; }
+        public IList<ReceiptItem> Items { get; set; }
+
+        public double Total()
+        {
+            double total = 0.0;
+
+            foreach(ReceiptItem item in Items)
+            {
+                total += item.Price * item.Quantity;
+            }
+
+            return total;
+        }
+    }
+}
